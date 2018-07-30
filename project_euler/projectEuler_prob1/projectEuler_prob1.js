6a220eccb22f5b589168ea1457d424e0afba8714
Gonçalo Borges <joaogoncalo.borges@gmail.com>
2018-07-30 13:18:17 +0100

    project euler prob 1 solution

--- project_euler/projectEuler_prob1/projectEuler_prob1.js
+++ project_euler/projectEuler_prob1/projectEuler_prob1.js
@@ -6,6 +6,16 @@
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
  *
  * Find the sum of all the multiples of 3 or 5 below 1000.
-*/
+ */
 
-// TODO just to test
+(function(){
+    var result = 0,
+        LOWER_LIMITE = 0,
+        UPPER_LIMITE = 1000;
+
+    for(var i=0; i<UPPER_LIMITE;i++)
+        if(i%3 == 0 || i%5 == 0)
+            result = result + i;
+
+    console.log(result);
+})();
